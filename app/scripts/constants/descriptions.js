47b356b758a4b31c46c90ec6555a5ec8a2d2c3b2
ginna-baker <ginna.baker@gmail.com>
2015-05-12 22:23:07 -0400

    constants service piped into main view

---
+++ app/scripts/constants/descriptions.js
@@ -0,0 +1,23 @@
+angular.module('jsmnApp')
+    .constant('descriptions', {
+        Norrell: {
+            name: 'Gilbert Norrell',
+            blurb: 'An elderly man of good heritage, he owns lands near the city of York, including his signature home of Hurtfew Abbey. With the objective of bringing back magic to England, Norrell impresses the Learned Society of York Magicians with a display of practical magic, which was thought to have been extinct for centuries, and sets off for London in order to use magic in the Napoleonic Wars in order to set up magic as a respected profession once again. Somewhat miserly of magic, he loathes "theoretical" magicians, or magio-historians, considering them to be second or third rate scholars. He is small, old-fashioned, wears an old fashioned wig, and has habits akin to those of a man who spends the larger part of his time alone.',
+            imageUrl: 'images/Norrell.gif'
+        },
+        Strange: {
+            name: 'Jonathan Strange',
+            blurb: 'Landowner and gentleman who comes from Shropshire. Husband of Arabella Strange, he becomes a magician upon hearing a prophecy about two magicians who would bring magic back to England. A typical English Gentleman, Strange is sociable, amiable, and a little arrogant. He is tall, has a long nose, slightly reddish hair, and has a habit of smiling sarcastically. More impatient than Norrell, Strange admires the Raven King, and desires magic to become widespread.',
+            imageUrl: 'images/Strange.jpg'
+        },
+        Arabella: {
+            name: 'Arabella Strange',
+            blurb: '(nee Woodhope): Wife of Jonathan Strange, and sister to Henry Woodhope. Strong willed and patient, she and her husband are known to often lightly quarrel, though they sincerely love each other very much. She is later stolen away by the Gentleman with Thistledown Hair to be a guest at his mansion in Faerie, but is rescued at the end of the book through the actions of Strange and Norrell. ',
+            imageUrl: 'images/Arabella.jpg'
+        },
+        Stephen: {
+            name: 'Stephen Black',
+            blurb: 'The Negro Butler of Sir Walter Pole. Intelligent and very handsome, he becomes the subject of the Gentleman with the Thistledown Hair\'s friendly attentions, and is usually seen attempting to placate some new wrath or insult the Gentleman perceives. Each and every night, he is summoned to Lost Hope, the Gentleman\'s Faerie Kingdom, to dance at the nightly balls and take part in the Faerie\'s enumerable parades and parties. He is the "Nameless Slave", one of the subjects of Vinculus\' prophecy. ',
+            imageUrl: 'images/Stephen.jpg'
+        }
+   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,18 @@
 'use strict';
-
-/**
- * @ngdoc function
- * @name jsmnApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the jsmnApp
- */
 angular.module('jsmnApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, descriptions) {
+        $scope.characters = {};
+        $scope.selectedCharacter = null;
+
+        for (var description in descriptions) {
+            $scope.characters[description] = descriptions[description];
+            console.log($scope.characters.Norrell);
+        };
+
+        $scope.selectCharacter = function (character) {
+            console.log('new selected char is: ' + character);
+            $scope.selectedCharacter = character;
+            return;
+        };
+
+    });
