cd146849446071961dc18756d407cd81bab251ac
ginna-baker <ginna.baker@gmail.com>
2015-06-22 22:51:54 -0400

    add timelines view/tab

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,5 @@
 'use strict';
 angular.module('jsmnApp')
     .controller('MainCtrl', function ($scope, descriptions) {
-        // $scope.characters = {};
-        // $scope.selectedCharacter = null;
-        // $scope.mouseEnter = false;
-
-        // for (var description in descriptions) {
-        //     $scope.characters[description] = descriptions[description];
-        // };
 
     });
--- app/scripts/directives/characters.js
+++ app/scripts/directives/characters.js
@@ -5,27 +5,20 @@ angular.module('jsmnApp')
     return {
       restrict: 'E',
       controller: function($element, $rootScope, $scope) {
-          // $scope._ = _
-          console.log('controller here')
-          return
+          return;
       },
       link: function postLink(scope, element, attrs) {
-        scope.text = "test";
-        scope.characters = {}
-        scope.activeChar = null
-
-        // descriptions = _.indexBy(descriptions, 'id')
+        scope.characters = {};
+        scope.activeChar = null;
 
         for (var description in descriptions) {
             scope.characters[description] = descriptions[description];
-            console.log(scope.characters);
         };
 
         scope.toggleCharacter = function (char) {
             scope.activeChar = char;
-            console.log('active char is', char.name);
-            return
-        }
+            return;
+        };
       }
     };
   });
